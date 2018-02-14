6f7d0b632be0c3f38714075c594d9a0696ef8931
William R. Arellano <william.arellano@yachaytech.edu.ec>
2018-02-14 18:12:13 -0500

    Added AddNews Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SQLite } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,6 +27,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     SQLite,
+    Toast,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/add-news/add-news.ts
+++ src/pages/add-news/add-news.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 /**
  * Generated class for the AddNewsPage page.
  *
@@ -15,11 +16,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddNewsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  data = { title:"", date:"", description:"" };
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private sqlite:SQLite, 
+    private toast: Toast) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddNewsPage');
+  }
+
+  saveNews() {
+    this.sqlite.create({
+      name: 'newsdb.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('INSERT INTO new VALUES(NULL,?,?,?)',
+    [this.data.title, this.data.date, this.data.description])
+    .then(res => {
+      console.log(res);
+      this.toast.show('News saved', '5000', 'center').subscribe(
+        toast => {
+          this.navCtrl.popToRoot();
+        }
+      );
+    })
+    .catch( e => {
+      console.log(e);
+      this.toast.show(e, '5000', 'center').subscribe( toast => {
+        console.log(toast);
+      }
+    );
+    });
+    }).catch( e => {
+      console.log(e);
+      this.toast.show(e, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
   }
 
 }
